47d3c8fed6beffa3f3cd37f2b874aa6e278dab11
Oliko Marghishvili <o.marghishvili@digitalarea.ge>
2022-03-03 11:08:03 -0800

    add temp data

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -23,6 +23,7 @@ namespace PlatformService
             services.AddRazorPages();
             services.AddDbContext<AppDbContext>(opt =>
                 opt.UseInMemoryDatabase("InMemoryDb"));
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +48,8 @@ namespace PlatformService
             {
                 endpoints.MapRazorPages();
             });
+
+            PrepDb.PrepPopulation(app);
         }
     }
 }
