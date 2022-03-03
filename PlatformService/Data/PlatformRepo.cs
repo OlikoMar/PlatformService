ed568a5a31b6eada66c2c168ef18620ff0cb9cf6
Oliko Marghishvili <o.marghishvili@digitalarea.ge>
2022-03-03 10:50:29 -0800

    implement data functions

--- PlatformService/Data/PlatformRepo.cs
+++ PlatformService/Data/PlatformRepo.cs
@@ -1,28 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public class PlatformRepo : IPlatformRepo
     {
+        private readonly AppDbContext _dbContext;
+
+        public PlatformRepo(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public bool SaveChanges()
         {
-            throw new System.NotImplementedException();
+            return _dbContext.SaveChanges() >= 0;
         }
 
         public IEnumerable<Platform> GetAllPlatforms()
         {
-            throw new System.NotImplementedException();
+            return _dbContext.Platforms;
         }
 
         public Platform GetPlatformById(int id)
         {
-            throw new System.NotImplementedException();
+            return _dbContext.Platforms.FirstOrDefault(s => s.Id == id);
         }
 
         public void CreatePlatform(Platform platform)
         {
-            throw new System.NotImplementedException();
+            if (platform is null) throw new ArgumentNullException(nameof(platform));
+
+            _dbContext.Platforms.Add(platform);
         }
     }
 }
