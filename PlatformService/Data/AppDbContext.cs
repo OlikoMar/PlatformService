3be3c3722002f0c0eac9f0ed2b9e775ea955a5fd
HP <HP@DESKTOP-UQPE64R>
2022-03-03 10:31:24 -0800

    Add project files.

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContext) : base(dbContext) { }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
