783ba5866a36d756660711337db591426c626833
Oliko Marghishvili <o.marghishvili@digitalarea.ge>
2022-03-07 09:17:09 -0800

    add automapper

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -5,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PlatformService.Data;
-
 namespace PlatformService
 {
     public class Startup
@@ -24,6 +24,7 @@ namespace PlatformService
             services.AddDbContext<AppDbContext>(opt =>
                 opt.UseInMemoryDatabase("InMemoryDb"));
             services.AddScoped<IPlatformRepo, PlatformRepo>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
